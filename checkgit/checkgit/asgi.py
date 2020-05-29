6cbb6150421118a3fe7a40eb50de8f7894d8cc2d
reddypradeep9 <pradeep.reddy7531@gmail.com>
2020-05-29 17:37:17 +0530

    app_and_projfolders

---
+++ checkgit/checkgit/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for checkgit project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkgit.settings')
+
+application = get_asgi_application()
